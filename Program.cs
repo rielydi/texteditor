caafb475c0e924bacc69e54c86c5fa7871d822a9
Rielydi Alves Dias <61101566+rielydi@users.noreply.github.com>
2023-08-01 23:19:32 +0000

    commit

--- Program.cs
+++ Program.cs
@@ -28,14 +28,45 @@ namespace textEditor
                 default: Menu(); break;
             }
 
-            static void Abrir()
-            {
-
-            }
-            static void Editar()
-            {
-
-            }
+            
         }
+    
+        static void Abrir()
+        
+        {
+
+        }
+    
+        static void Editar()
+
+        {
+            Console.Clear();
+            Console.WriteLine("Digite seu texto aqui: (ESC para sair!) ");
+            Console.WriteLine("-----------------------");
+            string text = "";
+
+            // no 'do' é faça isso enquanto o usuario não pressione isso: ou seja não pressione a tecla escape que é o esc
+            do
+            {
+                /*então é pegar o que ele digitou e colocar na variavel texto que é a text.
+                  lembando de usar o += depois do text, pq se não ele substituirá as linhas ao escrever se deixar só o =
+                  e com o sinal de + ele junta, ou seja, concatena.              
+                */                 
+                text += Console.ReadLine(); //pra ler uma linha de texto que o usuario digitou.
+                text += Environment.NewLine; // é pro usuario continuar digitando e quebrando linha, se não ele fica tudo junto. ta colocando uma nova linha. quebrando a linha no fim de cada leitura.
+                
+
+            }
+            //pra sempre armazenar algo antes de executar o While, usamos o Do
+            while (Console.ReadKey().Key != ConsoleKey.Escape);  //while é usado para repetir um bloco de código enquanto uma condição é verdadeira, o switch é usado para tomar decisões com base em diferentes valores de uma expressão.
+            // o consolekey.escape quer dizer que o usuario apertou a tecla esc. então ele não sairá do texto até apertar esc.
+            Console.WriteLine(text);
+
+        }
+    
+        static void Salvar()
+        {
+            
+        }   
     }
 }
