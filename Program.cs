4f0962d102c6d3e7f8c522ab6ee1cf9eab92d9c4
AUTOGLASS\rielydi.dias <rielydi.dias@autoglass.com.br>
2023-08-02 13:40:48 -0300

    commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace textEditor
 {
@@ -28,15 +29,15 @@ namespace textEditor
                 default: Menu(); break;
             }
 
-            
+
         }
-    
+
         static void Abrir()
-        
+
         {
 
         }
-    
+
         static void Editar()
 
         {
@@ -51,10 +52,10 @@ namespace textEditor
                 /*então é pegar o que ele digitou e colocar na variavel texto que é a text.
                   lembando de usar o += depois do text, pq se não ele substituirá as linhas ao escrever se deixar só o =
                   e com o sinal de + ele junta, ou seja, concatena.              
-                */                 
+                */
                 text += Console.ReadLine(); //pra ler uma linha de texto que o usuario digitou.
                 text += Environment.NewLine; // é pro usuario continuar digitando e quebrando linha, se não ele fica tudo junto. ta colocando uma nova linha. quebrando a linha no fim de cada leitura.
-                
+
 
             }
             //pra sempre armazenar algo antes de executar o While, usamos o Do
@@ -63,10 +64,20 @@ namespace textEditor
             Console.WriteLine(text);
 
         }
-    
-        static void Salvar()
+
+        static void Salvar( string text) //o string text a gente vai receber dentro do parametro salvar para poder salvar o arquivo.
         {
-            
-        }   
+            Console.Clear();
+            Console.WriteLine("Qual caminho para salvar o arquivo? ");
+            var path = Console.ReadLine(); //o Path é caminho ou seja, criando a variavel caminho. vai ser VAR pq o caminho pode ser qualquer um
+           //toda vez que a gente trabalha com arquivos nós precisamos abrir e fechar o arquivo. se abrir e deixar aberto, outra pessoa não consegue abrir.
+           //podemos abrir um arquivo usando o StreamReader e pode abrir um arquivo para escrita usando o StreamWriter
+           //ou seja, utilizando o Using() o que ele faz ?- ele abre e fecha o arquivo. todo objeto que passar dentro do Using() ele vai abrir e fechar o objeto automaticamente. ele vai criar usar e ja fechar.
+           using (var file = new StreamWriter(path)) //file=arquivo stream= fluxo. Writer=escrita. (StreamWriter= fluxo de escrita.) 
+           //o StreamWriter, sempre vai pedir um caminho para o arquivo. que no caso aqui é o path
+            {
+                file.Write(text); // o que a gente quer escrever dentro desse arquivo, o nosso text
+            }
+        }
     }
 }
